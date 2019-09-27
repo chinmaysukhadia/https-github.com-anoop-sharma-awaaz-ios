649c6efc9d01905c27ef712c195038e0dd758030
Chinmay Sukhadia <chinmay221777@gmail.com>
2019-09-27 09:38:54 +0530

    Initial Commit

---
+++ Aawaz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aawaz
+//
+//  Created by Chinmay Sukhadia on 27/09/19.
+//  Copyright © 2019 Terasol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
